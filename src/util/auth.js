0f066b0f19f521bb9ab41ff51722660728920bb1
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-01 22:12:02 +0700

    Auth: Populate config object
    Add config options specific to app and obtained via the Firebase console.

--- src/util/auth.js
+++ src/util/auth.js
@@ -2,10 +2,14 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-// TODO: Replace the following with your app's Firebase project configuration
-
 const firebaseConfig = {
-  // ...
+  apiKey: 'AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0',
+  authDomain: 'onit-aaa6e.firebaseapp.com',
+  projectId: 'onit-aaa6e',
+  storageBucket: 'onit-aaa6e.appspot.com',
+  messagingSenderId: '451988465101',
+  appId: '1:451988465101:web:335beb05b535b1257d9084',
+  measurementId: 'G-CB3S63219R'
 };
 
 // Initialize Firebase
