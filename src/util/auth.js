803d92f69e158962ebcaf36b5ca87177a0dba83f
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-01 22:07:36 +0700

    Auth: Import required packages
    Following the documentation:
    - Add auth and firebase packages to the file.
    - Create config object.
    - Initialise Firebase.

---
+++ src/util/auth.js
@@ -0,0 +1,12 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
