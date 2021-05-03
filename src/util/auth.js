71d4294b4345199f073873f9f7344d01a9c264f4
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-03 10:53:34 +0700

    Auth: Put all logic into useEffect hook
    Prevent additional re-render during development that causes reinitialisation error by wrapping all logic in a useEffect hook.

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,35 +1,39 @@
+import { useEffect } from "react";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
 import "firebase/auth";
 import * as firebaseui from 'firebaseui';
 
+
 function Authentication() {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0',
-    authDomain: 'onit-aaa6e.firebaseapp.com',
-    projectId: 'onit-aaa6e',
-    storageBucket: 'onit-aaa6e.appspot.com',
-    messagingSenderId: '451988465101',
-    appId: '1:451988465101:web:335beb05b535b1257d9084',
-    measurementId: 'G-CB3S63219R'
-  };
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0',
+      authDomain: 'onit-aaa6e.firebaseapp.com',
+      projectId: 'onit-aaa6e',
+      storageBucket: 'onit-aaa6e.appspot.com',
+      messagingSenderId: '451988465101',
+      appId: '1:451988465101:web:335beb05b535b1257d9084',
+      measurementId: 'G-CB3S63219R'
+    };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-  // Initialize the FirebaseUI Widget using Firebase.
-  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // Initialize the FirebaseUI Widget using Firebase.
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-    signInSuccessUrl: 'https://onit-main.web.app/',
-    // Future config options...
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'https://onit-main.web.app/',
+      // Future config options...
+    });
+
+    console.log('auth script executed...');
   });
-
-  console.log('auth script executed...');
 }
 
 export default Authentication;
