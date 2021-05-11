4da6b786a8bd02820537eacf5ff4ef8eee266069
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-11 15:31:53 +0700

    routesConfig: Add auth handlers
    Add authentication and authorization handlers to the post request before user creation.

--- functions/src/users/routes-config.ts
+++ functions/src/users/routes-config.ts
@@ -1,9 +1,13 @@
 import { Application } from "express";
 import { create } from "./controller";
+import { isAuthenticated } from "../auth/authenticated";
+import { isAuthorized } from "../auth/authorized";
 
 
 export function routesConfig(app: Application) {
    app.post('/users',
+       isAuthenticated,
+       isAuthorized({ hasRole: ['admin', 'manager'] }),
        create
    );
 }
