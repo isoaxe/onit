f79785b94cef098b9208534b1e5c0d1a673f6920
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-11 13:45:25 +0700

    Index: Create express app for Firebase functions
    Import required node packages.
    Initialise the firebase-admin SDK.
    Create an instance of express.
    Implement the cors and body-parser packages.
    Expose the express app to Firebase via a function.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,19 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+
+
+// Initialise the firebase-admin SDK in order to access its services.
+admin.initializeApp();
+
+const app = express();
+
+// Automatically allow cross-origin requests.
+app.use(cors({ origin: true }));
+
+app.use(bodyParser.json());
+
+// Expose Express API as a single Cloud Function.
+export const api = functions.https.onRequest(app);
