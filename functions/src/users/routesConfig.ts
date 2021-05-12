53a8dd7e3187fd2aa32801165bee4bc1cf7d700d
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-12 11:31:22 +0700

    routersConfig: Add additional routes
    Make additional routes for the various http verbs.

--- functions/src/users/routesConfig.ts
+++ functions/src/users/routesConfig.ts
@@ -5,9 +5,34 @@ import { isAuthorized } from "../auth/authorized";
 
 
 export function routesConfig(app: Application) {
+  // Create a new user.
    app.post('/users',
        isAuthenticated,
        isAuthorized({ hasRole: ['admin', 'manager'] }),
        create
    );
+   // Lists all users.
+   app.get('/users', [
+       isAuthenticated,
+       isAuthorized({ hasRole: ['admin', 'manager'] }),
+       all
+   ]);
+   // Get :id user.
+   app.get('/users/:id', [
+       isAuthenticated,
+       isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+       get
+   ]);
+   // Updates :id user.
+   app.patch('/users/:id', [
+       isAuthenticated,
+       isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+       patch
+   ]);
+   // Deletes :id user.
+   app.delete('/users/:id', [
+       isAuthenticated,
+       isAuthorized({ hasRole: ['admin', 'manager'] }),
+       remove
+   ]);
 }
