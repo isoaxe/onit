18d8df39b39bf25a44bb6e3fa03c290d9cdef609
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-14 17:59:02 +0700

    routesConfig: Lint the file

--- functions/src/users/routesConfig.ts
+++ functions/src/users/routesConfig.ts
@@ -4,35 +4,35 @@ import { isAuthenticated } from "../auth/authenticated";
 import { isAuthorized } from "../auth/authorized";
 
 
-export function routesConfig(app: Application) {
+export function routesConfig (app: Application): void {
   // Create a new user.
-   app.post('/users',
+   app.post("/users",
        isAuthenticated,
-       isAuthorized({ hasRole: ['admin', 'manager'] }),
+       isAuthorized({ hasRole: ["admin", "manager"] }),
        create
    );
    // Lists all users.
-   app.get('/users', [
+   app.get("/users", [
        isAuthenticated,
-       isAuthorized({ hasRole: ['admin', 'manager'] }),
+       isAuthorized({ hasRole: ["admin", "manager"] }),
        all
    ]);
    // Get :id user.
-   app.get('/users/:id', [
+   app.get("/users/:id", [
        isAuthenticated,
-       isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+       isAuthorized({ hasRole: ["admin", "manager"], allowSameUser: true }),
        get
    ]);
    // Updates :id user.
-   app.patch('/users/:id', [
+   app.patch("/users/:id", [
        isAuthenticated,
-       isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+       isAuthorized({ hasRole: ["admin", "manager"], allowSameUser: true }),
        patch
    ]);
    // Deletes :id user.
-   app.delete('/users/:id', [
+   app.delete("/users/:id", [
        isAuthenticated,
-       isAuthorized({ hasRole: ['admin', 'manager'] }),
+       isAuthorized({ hasRole: ["admin", "manager"] }),
        remove
    ]);
 }
