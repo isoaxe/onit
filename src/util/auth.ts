588edffbbb77449d67d69a1d9251b08420c3a425
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-14 18:18:17 +0700

    Auth: Lint the file

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,33 +1,33 @@
 import { useEffect } from "react";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
-import { auth } from 'firebaseui';
-import 'firebaseui/dist/firebaseui.css';
+import { auth } from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
 
 
-function Authentication() {
+function Authentication (): null {
   useEffect(() => {
     const firebaseConfig = {
-      apiKey: 'AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0',
-      authDomain: 'onit-aaa6e.firebaseapp.com',
-      projectId: 'onit-aaa6e',
-      storageBucket: 'onit-aaa6e.appspot.com',
-      messagingSenderId: '451988465101',
-      appId: '1:451988465101:web:335beb05b535b1257d9084',
-      measurementId: 'G-CB3S63219R'
+      apiKey: "AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0",
+      authDomain: "onit-aaa6e.firebaseapp.com",
+      projectId: "onit-aaa6e",
+      storageBucket: "onit-aaa6e.appspot.com",
+      messagingSenderId: "451988465101",
+      appId: "1:451988465101:web:335beb05b535b1257d9084",
+      measurementId: "G-CB3S63219R"
     };
 
     const uiConfig = {
       signInOptions: [
         {
           provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          buttonColor: '#4353FF'
+          buttonColor: "#4353FF"
         },
         firebase.auth.GoogleAuthProvider.PROVIDER_ID
       ],
-      signInSuccessUrl: 'https://onit-main.web.app/',
+      signInSuccessUrl: "https://onit-main.web.app/",
       // Future config options...
-    }
+    };
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -35,7 +35,7 @@ function Authentication() {
     // Initialize the FirebaseUI Widget using Firebase.
     const ui = new auth.AuthUI(firebase.auth());
 
-    ui.start('#firebaseui-auth-container', uiConfig);
+    ui.start("#firebaseui-auth-container", uiConfig);
   }, []);
   return null;
 }
