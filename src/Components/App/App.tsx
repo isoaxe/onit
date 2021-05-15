643fa345f5675f89745bbe561ef2efd875d8a539
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-15 12:48:57 +0700

    App: Correct return type
    Correct the return type of the React component. File and indeed entire project outside of the functions directory now compiles successfully.

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,7 +3,7 @@ import logo from "./../../logo.svg";
 import "./App.css";
 
 
-function App (): void {
+function App (): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
