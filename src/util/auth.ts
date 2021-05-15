090842a300a8338b8f7bc66074d39b96d5ee0b6d
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-15 18:12:54 +0700

    Fix indentation for all files
    Run the lint script with the --fix option to correct the indentation for all files.

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -6,38 +6,38 @@ import "firebaseui/dist/firebaseui.css";
 
 
 function Authentication (): null {
-  useEffect(() => {
-    const firebaseConfig = {
-      apiKey: "AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0",
-      authDomain: "onit-aaa6e.firebaseapp.com",
-      projectId: "onit-aaa6e",
-      storageBucket: "onit-aaa6e.appspot.com",
-      messagingSenderId: "451988465101",
-      appId: "1:451988465101:web:335beb05b535b1257d9084",
-      measurementId: "G-CB3S63219R"
-    };
+	useEffect(() => {
+		const firebaseConfig = {
+			apiKey: "AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0",
+			authDomain: "onit-aaa6e.firebaseapp.com",
+			projectId: "onit-aaa6e",
+			storageBucket: "onit-aaa6e.appspot.com",
+			messagingSenderId: "451988465101",
+			appId: "1:451988465101:web:335beb05b535b1257d9084",
+			measurementId: "G-CB3S63219R"
+		};
 
-    const uiConfig = {
-      signInOptions: [
-        {
-          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          buttonColor: "#4353FF"
-        },
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      ],
-      signInSuccessUrl: "https://onit-main.web.app/",
-      // Future config options...
-    };
+		const uiConfig = {
+			signInOptions: [
+				{
+					provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+					buttonColor: "#4353FF"
+				},
+				firebase.auth.GoogleAuthProvider.PROVIDER_ID
+			],
+			signInSuccessUrl: "https://onit-main.web.app/",
+			// Future config options...
+		};
 
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig);
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    const ui = new auth.AuthUI(firebase.auth());
+		// Initialize the FirebaseUI Widget using Firebase.
+		const ui = new auth.AuthUI(firebase.auth());
 
-    ui.start("#firebaseui-auth-container", uiConfig);
-  }, []);
-  return null;
+		ui.start("#firebaseui-auth-container", uiConfig);
+	}, []);
+	return null;
 }
 
 export default Authentication;
