db54ddf4b5a68606cd62ccea847bd9adbf231d94
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-18 16:12:11 +0700

    Login: Make basic login UI
    Remove the boilerplate CRA image and text. Add login text to instruct user. Temporary UI, to be refined later.

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,5 +1,4 @@
 import Authentication from "./../../util/auth";
-import logo from "./../../logo.svg";
 import "./Login.css";
 
 
@@ -7,18 +6,9 @@ function Login (): JSX.Element {
 	return (
 		<div className="Login">
 			<header className="Login-header">
-				<img src={logo} className="Login-logo" alt="logo" />
 				<p>
-          Edit <code>src/Login.tsx</code> and save to reload.
+          Choose your preferred login method.
 				</p>
-				<a
-					className="Login-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
 				<div id="firebaseui-auth-container"></div>
 				<Authentication />
 			</header>
