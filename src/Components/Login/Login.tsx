1382eaf1a29789f46d8613e7e5e505a3099ab799
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-20 18:00:29 +0700

    Create new Login Component
    Extract the logic for login from the App component.

---
+++ src/Components/Login/Login.tsx
@@ -0,0 +1,19 @@
+import Authentication from "./../../util/auth";
+import "./Login.css";
+
+
+function Login (): JSX.Element {
+	return (
+		<div>
+			<header className="Login-header">
+				<p>
+					Choose your preferred login method.
+				</p>
+				<div id="firebaseui-auth-container"></div>
+				<Authentication />
+			</header>
+		</div>
+	);
+}
+
+export default Login;
