ef3ecfd841d8308ccbe5ee306ca8d31656954082
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-20 12:00:31 +0700

    Index: Wrap root component with BrowserRouter
    In order to implement React Router, the root component (Login) needs to be wrapped in the BrowserRouter component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import Login from "./Components/Login/Login";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +9,9 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Login />
+		<BrowserRouter>
+			<Login />
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
