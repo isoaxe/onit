02b94df5d8b8188624ef6aed6048422dbfc90238
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-21 15:30:25 +0700

    Login: Move Authentication component
    Move the Authentication component so that it is outside of the header. This makes it clearer that it contains auth logic rather than displaying it in the DOM.

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -5,12 +5,12 @@ import "./Login.css";
 function Login (): JSX.Element {
 	return (
 		<div>
+			<Authentication />
 			<header className="Login-header">
 				<p>
 					Choose your preferred login method.
 				</p>
 				<div id="firebaseui-auth-container"></div>
-				<Authentication />
 			</header>
 		</div>
 	);
