994992998832e9da0f2d0dec4af57e10b1255dc4
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-23 14:41:53 +0700

    LoginSuccess: Use the /loggedin endpoint
    As a proof of concept, display the success message delivered at the '/loggedin' endpoint by the API.

--- src/Components/LoginSuccess/LoginSuccess.tsx
+++ src/Components/LoginSuccess/LoginSuccess.tsx
@@ -1,8 +1,19 @@
+import React, { useEffect } from "react";
+
 import logo from "./../../logo.svg";
 import "./LoginSuccess.css";
 
 
 function LoginSuccess (): JSX.Element {
+	useEffect(() => {
+		// This works in the local environment only.
+		// Need to detect if port is currently in use. If it is, fetch remote address:
+		// https://us-central1-onit-aaa6e.cloudfunctions.net/api/loggedin
+		fetch("http://localhost:5001/onit-aaa6e/us-central1/api/loggedin")
+			.then(res => res.json())
+			.then(data => console.log(data));
+	}, []);
+
 	return (
 		<div className="LoginSuccess">
 			<header className="LoginSuccess-header">
