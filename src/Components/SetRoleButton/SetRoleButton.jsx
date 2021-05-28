a4894b77e9f961040a2de31ffacbf24b48d07e36
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-28 13:07:35 +0700

    Create SetRoleButton.jsx
    Button to set the user role as a proof of concept. Will be removed after setting role has been completed successfully. Initially a GET request, will change to POST soon.

---
+++ src/Components/SetRoleButton/SetRoleButton.jsx
@@ -0,0 +1,31 @@
+import firebase from "firebase/app";
+
+
+function SetRoleButton () {
+
+	function setRole () {
+		firebase.auth().currentUser.getIdToken(true).then(function (token) {
+			console.log("processing client request...");
+			const requestOptions = {
+				method: "GET",
+				headers: { authorization: `Bearer ${token}` }
+			};
+			// This works in the local environment only.
+			// Need to detect if port is currently in use. If it is, fetch remote address:
+			// https://us-central1-onit-aaa6e.cloudfunctions.net/api/loggedin/setrole
+			fetch("http://localhost:5001/onit-aaa6e/us-central1/api/loggedin/setrole", requestOptions)
+				.then(res => res.json())
+				.then(data => console.log(data));
+		}).catch(function (error) {
+			console.log(`Uh oh. An error occured: ${error}`);
+		});
+	}
+
+	return (
+		<button onClick={setRole}>
+			Set Role
+		</button>
+	);
+}
+
+export default SetRoleButton;
