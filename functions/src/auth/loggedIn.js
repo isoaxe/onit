3705c49779fe6bf50b74119735dbc4df56396f8d
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-05-28 12:52:49 +0700

    LoggedIn: Change response message
    Change response message to be more descriptive. Add a comment.

--- functions/src/auth/loggedIn.js
+++ functions/src/auth/loggedIn.js
@@ -3,8 +3,9 @@ import { Router } from "express";
 const router = Router();
 
 
+// Simple GET request to /loggedin in order to test API.
 router.get("/", (req, res) => {
-	res.status(200).send({ message: "success" });
+	res.status(200).send({ message: "Login successful" });
 });
 
 export default router;
