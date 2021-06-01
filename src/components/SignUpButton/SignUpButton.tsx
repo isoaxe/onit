26eb3fe5f39a0e362793a94ee71f322882cd157c
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-01 14:35:44 +0700

    Create SignUpButton
    Create and style a button to sign new users up.

---
+++ src/components/SignUpButton/SignUpButton.tsx
@@ -0,0 +1,12 @@
+import "./SignUpButton.css";
+
+
+function SignUpButton (): JSX.Element {
+	return (
+		<div className="SignUp">
+			Create Account
+		</div>
+	);
+}
+
+export default SignUpButton;
