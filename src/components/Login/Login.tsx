0fd992534b814c015da3ea95931e78d196579a55
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-02 15:27:57 +0700

    Login: Add functionality to 'sign-up' buttons
    Add state to describe if the user or business sign-up forms should be displayed. Default to false, but set to true when the respective sign-up buttons are clicked.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import AuthUI from "./../AuthUI/AuthUI";
 import SignUpButton from "./../SignUpButton/SignUpButton";
 import "./Login.css";
 
 
 function Login (): JSX.Element {
+	const [userFormActive, setUserFormActive] = useState(false);
+	const [businessFormActive, setBusinessFormActive] = useState(false);
+
 	return (
 		<div>
 			<AuthUI />
@@ -11,8 +15,8 @@ function Login (): JSX.Element {
 				<p>Login</p>
 				<div id="firebaseui-auth-container"></div>
 				<p>Sign Up</p>
-				<SignUpButton label="Create User Account"/>
-				<SignUpButton label="Create Business Account"/>
+				<SignUpButton label="Create User Account" onClick={() => setUserFormActive(true)} />
+				<SignUpButton label="Create Business Account" onClick={() => setBusinessFormActive(true)} />
 			</header>
 		</div>
 	);
