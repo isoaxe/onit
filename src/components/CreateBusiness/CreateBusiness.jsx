7c3d6c871a4f5a3b6a090616cf9dc6f60069588d
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-03 10:23:54 +0700

    CreateBusiness: Add event handlers
    Add event handlers for setting state in each of the form fields. Can't set state directly as previously attempted.

--- src/components/CreateBusiness/CreateBusiness.jsx
+++ src/components/CreateBusiness/CreateBusiness.jsx
@@ -13,6 +13,38 @@ function CreateBusiness () {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	function handleBusiness (event) {
+		setBusinessName(event.target.value);
+	}
+
+	function handleAddress1 (event) {
+		setAddress1(event.target.value);
+	}
+
+	function handleAddress2 (event) {
+		setAddress2(event.target.value);
+	}
+
+	function handleCity (event) {
+		setCity(event.target.value);
+	}
+
+	function handlePostcode (event) {
+		setPostcode(event.target.value);
+	}
+
+	function handlePhone (event) {
+		setPhone(event.target.value);
+	}
+
+	function handleEmail (event) {
+		setEmail(event.target.value);
+	}
+
+	function handlePassword (event) {
+		setPassword(event.target.value);
+	}
+
 	function validateForm () {
 		console.log("temp validation placeholder");
 	}
@@ -20,14 +52,14 @@ function CreateBusiness () {
 	return (
 		<form action="/createbusiness" method="POST" onSubmit={validateForm} className="form" name="business-form">
 			<header className="header">Create Account</header>
-			<input id="business-name" value={businessName} onChange={setBusinessName} className="input-field" type="text" placeholder="Business name" />
-			<input id="address1" value={address1} onChange={setAddress1} className="input-field" type="text" placeholder="Address line 1"/>
-			<input id="address2" value={address2} onChange={setAddress2} className="input-field" type="text" placeholder="Address line 2"/>
-			<input id="city" value={city} onChange={setCity} className="input-field" type="text" placeholder="City"/>
-			<input id="postcode" value={postcode} onChange={setPostcode} className="input-field" type="text" placeholder="Postcode"/>
-			<input id="phone" value={phone} onChange={setPhone} className="input-field" type="number" placeholder="Phone number"/>
-			<input id="email" value={email} onChange={setEmail} className="input-field" type="text" placeholder="Email address"/>
-			<input id="password" value={password} onChange={setPassword} className="input-field" type="text" placeholder="Password"/>
+			<input id="business-name" value={businessName} onChange={handleBusiness} className="input-field" type="text" placeholder="Business name" />
+			<input id="address1" value={address1} onChange={handleAddress1} className="input-field" type="text" placeholder="Address line 1"/>
+			<input id="address2" value={address2} onChange={handleAddress2} className="input-field" type="text" placeholder="Address line 2"/>
+			<input id="city" value={city} onChange={handleCity} className="input-field" type="text" placeholder="City"/>
+			<input id="postcode" value={postcode} onChange={handlePostcode} className="input-field" type="text" placeholder="Postcode"/>
+			<input id="phone" value={phone} onChange={handlePhone} className="input-field" type="number" placeholder="Phone number"/>
+			<input id="email" value={email} onChange={handleEmail} className="input-field" type="text" placeholder="Email address"/>
+			<input id="password" value={password} onChange={handlePassword} className="input-field" type="text" placeholder="Password"/>
 			<button type="submit" className="submit">Submit</button>
 		</form>
 	);
