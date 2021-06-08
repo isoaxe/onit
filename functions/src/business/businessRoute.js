a33b698ec9e72d4c13fcf1a740a62f5a17495374
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-08 17:56:53 +0700

    businessRoute: Refactor form data variables
    Refactor to make more concise and DRY.

--- functions/src/business/businessRoute.js
+++ functions/src/business/businessRoute.js
@@ -7,16 +7,17 @@ const router = Router();
 // Create business user with POST request to /business.
 router.post("/business", async (req, res) => {
 	const role = "company";
+	const { displayName, phoneNumber, email, password, address1, address2, city, postcode } = req.body;
 
 	// Generate a 6-digit businessId:
 	const businessId = Math.round(Math.random()*1000000);
 	/* Add logic here to ensure that the businessId is unique */
 
 	const { uid } = await admin.auth().createUser({
-		displayName: req.body.displayName,
-		phoneNumber: req.body.phoneNumber,
-		email: req.body.email,
-		password: req.body.password
+		displayName,
+		phoneNumber,
+		email,
+		password
 	});
 
 	admin.auth().setCustomUserClaims(uid, { role: role, id: businessId });
@@ -25,12 +26,12 @@ router.post("/business", async (req, res) => {
 	const db = admin.firestore();
 	const doc = db.collection("users").doc(`businessId-${businessId}`);
 	doc.set({
-		uid: uid,
-		role: role,
-		address1: req.body.address1,
-		address2: req.body.address2,
-		city: req.body.city,
-		postcode: req.body.postcode,
+		uid,
+		role,
+		address1,
+		address2,
+		city,
+		postcode,
 	});
 
 	res.status(200).send({ message: "Company account created" });
