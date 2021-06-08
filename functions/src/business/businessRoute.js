137e3c4689a826d30614cbde8f458d51f1e1b9de
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-08 17:36:13 +0700

    businessRoute: Refactor as async/await
    Replace the chained .then() promises with async / await. Much more legible and allows the use of uid throughout the whole request.

--- functions/src/business/businessRoute.js
+++ functions/src/business/businessRoute.js
@@ -5,34 +5,33 @@ const router = Router();
 
 
 // Create business user with POST request to /business.
-router.post("/business", (req, res) => {
+router.post("/business", async (req, res) => {
 	// Generate a 6-digit businessId:
 	const businessId = Math.round(Math.random()*1000000);
 	/* Add logic here to ensure that the businessId is unique */
 
-	admin.auth().createUser({
+	const { uid } = await admin.auth().createUser({
 		displayName: req.body.businessName,
 		phoneNumber: req.body.phone,
 		email: req.body.email,
 		password: req.body.password
-	})
-		.then((res) => {
-			admin.auth().setCustomUserClaims(res.uid, { role: "company", id: businessId });
-		})
-		.then(() => {
-			// Not all required user data can be stored by auth. Use Firestore instead.
-			const db = admin.firestore();
-			const doc = db.collection("users").doc(`businessId-${businessId}`);
-			doc.set({
-				//uid: res.uid, /* uid cannot be accessed here and cannot be passed down the promise chain */
-				role: "company",
-				address1: req.body.address1,
-				address2: req.body.address2,
-				city: req.body.city,
-				postcode: req.body.postcode,
-			});
-		})
-		.then(res.status(200).send({ message: "Company account created" }));
+	});
+
+	admin.auth().setCustomUserClaims(uid, { role: "company", id: businessId });
+
+	// Not all required user data can be stored by auth. Use Firestore instead.
+	const db = admin.firestore();
+	const doc = db.collection("users").doc(`businessId-${businessId}`);
+	doc.set({
+		uid: uid,
+		role: "company",
+		address1: req.body.address1,
+		address2: req.body.address2,
+		city: req.body.city,
+		postcode: req.body.postcode,
+	});
+
+	res.status(200).send({ message: "Company account created" });
 });
 
 // Standard error helper function.
