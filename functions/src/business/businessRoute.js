e6bf61171f456d4125aa063f09945e438c85caa0
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-08 17:41:08 +0700

    businessRoute: Extract role as variable
    Assign role to variable so that one instance cannot get changed without the other.

--- functions/src/business/businessRoute.js
+++ functions/src/business/businessRoute.js
@@ -6,6 +6,8 @@ const router = Router();
 
 // Create business user with POST request to /business.
 router.post("/business", async (req, res) => {
+	const role = "company";
+	
 	// Generate a 6-digit businessId:
 	const businessId = Math.round(Math.random()*1000000);
 	/* Add logic here to ensure that the businessId is unique */
@@ -17,14 +19,14 @@ router.post("/business", async (req, res) => {
 		password: req.body.password
 	});
 
-	admin.auth().setCustomUserClaims(uid, { role: "company", id: businessId });
+	admin.auth().setCustomUserClaims(uid, { role: role, id: businessId });
 
 	// Not all required user data can be stored by auth. Use Firestore instead.
 	const db = admin.firestore();
 	const doc = db.collection("users").doc(`businessId-${businessId}`);
 	doc.set({
 		uid: uid,
-		role: "company",
+		role: role,
 		address1: req.body.address1,
 		address2: req.body.address2,
 		city: req.body.city,
