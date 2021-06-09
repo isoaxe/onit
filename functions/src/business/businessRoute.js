1dc46f064342adb8e0c9baaacdc1bf4cb0e5feae
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-09 11:08:14 +0700

    businessRoute: Wrap route in try / catch block
    Wrap the entire route in a try / catch block so that errors are handled correctly.

--- functions/src/business/businessRoute.js
+++ functions/src/business/businessRoute.js
@@ -6,44 +6,48 @@ const router = Router();
 
 // Create business user with POST request to /business.
 router.post("/business", async (req, res) => {
-	const role = "company";
-	const { displayName, phoneNumber, email, password, address1, address2, city, postcode } = req.body;
+	try {
+		const role = "company";
+		const { displayName, phoneNumber, email, password, address1, address2, city, postcode } = req.body;
 
-	// Check that businessId is unique by querying the Firestore for current ids.
-	const ids = [];
-	const db = admin.firestore();
-	const users = db.collection("users");
-	const businessIds = await users.get();
-	businessIds.forEach(doc => {
-		const id = doc.id.split("businessId-")[1];
-		ids.push(id);
-	});
-	let businessId = getBusinessId();
-	while (ids.includes(businessId)) {
-		businessId = getBusinessId;
+		// Check that businessId is unique by querying the Firestore for current ids.
+		const ids = [];
+		const db = admin.firestore();
+		const users = db.collection("users");
+		const businessIds = await users.get();
+		businessIds.forEach(doc => {
+			const id = doc.id.split("businessId-")[1];
+			ids.push(id);
+		});
+		let businessId = getBusinessId();
+		while (ids.includes(businessId)) {
+			businessId = getBusinessId;
+		}
+
+		const { uid } = await admin.auth().createUser({
+			displayName,
+			phoneNumber,
+			email,
+			password
+		});
+
+		admin.auth().setCustomUserClaims(uid, { role, businessId });
+
+		// Not all required user data can be stored by auth. Use Firestore instead.
+		const user = db.collection("users").doc(`businessId-${businessId}`);
+		user.set({
+			uid,
+			role,
+			address1,
+			address2,
+			city,
+			postcode,
+		});
+
+		res.status(200).send({ message: "Company account created" });
+	} catch (err) {
+		handleError(res, err);
 	}
-
-	const { uid } = await admin.auth().createUser({
-		displayName,
-		phoneNumber,
-		email,
-		password
-	});
-
-	admin.auth().setCustomUserClaims(uid, { role, businessId });
-
-	// Not all required user data can be stored by auth. Use Firestore instead.
-	const user = db.collection("users").doc(`businessId-${businessId}`);
-	user.set({
-		uid,
-		role,
-		address1,
-		address2,
-		city,
-		postcode,
-	});
-
-	res.status(200).send({ message: "Company account created" });
 });
 
 // Standard error helper function.
