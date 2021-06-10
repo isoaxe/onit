abd634a9311793571c85f40d8474bd40cdf2f0a8
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-10 15:59:40 +0700

    Create helpers.js
    Created the helpers.js utility file.
    The fetch request in CreateBusiness was frequently throwing an error. Need to change implementation.
    Added the postFormDataAsJson function to help with the new account creation POST request.

---
+++ src/util/helpers.js
@@ -0,0 +1,21 @@
+// Used to POST account creation data without using form attributes.
+export async function postFormDataAsJson ({ url, formData }) {
+	const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+	const fetchConfig = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: formDataJsonString
+	};
+
+	const response = await fetch(url, fetchConfig);
+	if (!response.ok) {
+		const errorMessage = await response.text();
+		throw new Error(errorMessage);
+	}
+
+	return response.json();
+}
