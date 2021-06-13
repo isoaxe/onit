accc4781e883928a7c3faf0fc5d9509d08d35947
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-13 09:23:29 +0700

    Change filename to urls
    Change the constants filename to urls as this is much better description.
    Also added a comment at the top of the file to explain what it does.

--- src/util/urls.js
+++ src/util/urls.js
@@ -1,8 +1,13 @@
+/*
+ * Set URLS dynamically based on environment and host.
+ */
+
 // Set API_URL based on whether local hosted emulator is running or not.
 const localApiUrl = "http://localhost:5001/onit-aaa6e/us-central1/api";
 const remoteApiUrl = "https://us-central1-onit-aaa6e.cloudfunctions.net/api";
 
 export const API_URL = ((window.location.hostname === "localhost") ? localApiUrl : remoteApiUrl);
+
 
 // Set BASE_URL based on whether local hosted emulator is running or not.
 // If running locally, also detect whether in development or production environment.
