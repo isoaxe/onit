d28ea4ef337f961602a212312192c004b285634f
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-13 09:51:26 +0700

    Login: Implement CSS as object
    Add styling to the JSX via an object within the file. The external CSS file can be deleted.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,7 @@ import AuthUI from "./../../components/AuthUI/AuthUI";
 import SignUpButton from "./../../components/SignUpButton/SignUpButton";
 import CreateUser from "./../../components/CreateUser/CreateUser";
 import CreateBusiness from "./../../components/CreateBusiness/CreateBusiness";
-import "./Login.css";
+import { StyleSheet } from "./../../util/types";
 
 
 function Login (): JSX.Element {
@@ -21,12 +21,12 @@ function Login (): JSX.Element {
 	}
 
 	return (
-		<div className="root">
+		<div style={styles.root}>
 			<AuthUI />
-			<header className="login-header">
-				<p className="title">Login</p>
-				<div id="firebaseui-auth-container"></div>
-				<p className="title">Sign Up</p>
+			<header style={styles.loginHeader}>
+				<p style={styles.title}>Login</p>
+				<div id="firebaseui-auth-container" style={styles.auth}></div>
+				<p style={styles.title}>Sign Up</p>
 				<SignUpButton label="Create User Account" onClick={userForm} />
 				<SignUpButton label="Create Business Account" onClick={businessForm} />
 			</header>
@@ -35,5 +35,39 @@ function Login (): JSX.Element {
 		</div>
 	);
 }
+
+const styles: StyleSheet = {
+	root: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		minHeight: "100vh",
+		backgroundColor: "#282C34",
+	},
+	loginHeader: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#3F4552",
+		border: "2px solid #3f524c",
+		borderRadius: "10px",
+		paddingBottom: "25px",
+		marginBottom: "30px",
+		marginTop: "10px",
+		fontSize: "20px",
+		color: "white"
+	},
+	title: {
+		fontFamily: "Lato-Black"
+	},
+	auth: {
+		marginTop: "-15px",
+		paddingBottom: "10px",
+		borderBottom: "3px solid #3f524c"
+	}
+};
+
 
 export default Login;
