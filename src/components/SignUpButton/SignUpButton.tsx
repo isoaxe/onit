8e0e823282e8c58bd527bc6e47296f2f564191e8
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-14 13:03:13 +0700

    SignUpButton: Implement CSS as styled component
    Refactor the button div as a styled <Button> component. Wrapping this in a <Wrapper> styled component allows the emulation of hover (and other pseudo-selectors). The external CSS file can be deleted.

--- src/components/SignUpButton/SignUpButton.tsx
+++ src/components/SignUpButton/SignUpButton.tsx
@@ -1,12 +1,32 @@
-import "./SignUpButton.css";
+import styled from "styled-components";
 
 
 function SignUpButton (props): JSX.Element {
 	return (
-		<div className="sign-up" onClick={props.onClick}>
-			{props.label}
-		</div>
+		<Wrapper>
+			<Button onClick={props.onClick}>
+				{props.label}
+			</Button>
+		</Wrapper>
 	);
 }
+
+const Button = styled.div`
+	background-color: #3F524C;
+	box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+	border-radius: 4px;
+	text-align: center;
+	font-size: 14px;
+	padding: 10px;
+	margin-bottom: 12px;
+`;
+
+const Wrapper = styled.div`
+	&:hover ${Button} {
+		background-color: #4A615A;
+		cursor: pointer;
+	}
+`;
+
 
 export default SignUpButton;
