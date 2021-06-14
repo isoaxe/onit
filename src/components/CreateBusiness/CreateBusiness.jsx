bcbe0bab38edab2f36678b157ab4afc77af79018
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-14 16:14:28 +0700

    CreateBusiness: Implement CSS as object
    + Add styling to the JSX via an object within the file.
    + Remove all class names and replace with style attributes.
    + Where multiple selectors are required, make a deep copy and combine.
    + Make a button with styled-components that has hover function.
    + The seperate CSS file can be deleted.

--- src/components/CreateBusiness/CreateBusiness.jsx
+++ src/components/CreateBusiness/CreateBusiness.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
 import firebase from "firebase/app";
+import styled from "styled-components";
 import { postFormDataAsJson } from "./../../util/helpers";
 import { API_URL } from "./../../util/urls";
-
-import "./CreateBusiness.css";
 
 
 function CreateBusiness () {
@@ -77,21 +76,70 @@ function CreateBusiness () {
 
 	return (
 		<div>
-			<form onSubmit={createBusiness} className="form">
-				<header className="header">Create Account</header>
-				<input id="business-name" value={businessName} onChange={handleBusiness} className="input-field" type="text" placeholder="Business name" name="displayName"/>
-				<input id="address1" value={address1} onChange={handleAddress1} className="input-field" type="text" placeholder="Address line 1" name="address1"/>
-				<input id="address2" value={address2} onChange={handleAddress2} className="input-field" type="text" placeholder="Address line 2" name="address2"/>
-				<input id="city" value={city} onChange={handleCity} className="input-field" type="text" placeholder="City" name="city"/>
-				<input id="postcode" value={postcode} onChange={handlePostcode} className="input-field" type="text" placeholder="Postcode" name="postcode"/>
-				<input id="phone" value={phone} onChange={handlePhone} className="input-field" type="number" placeholder="Phone number" name="phoneNumber"/>
-				<input id="email" value={email} onChange={handleEmail} className="input-field" type="text" placeholder="Email address" name="email"/>
-				<input id="password" value={password} onChange={handlePassword} className="input-field" type="text" placeholder="Password" name="password"/>
-				<button type="submit" className="submit">Submit</button>
+			<form onSubmit={createBusiness} style={styles.form}>
+				<header style={styles.header}>Create Account</header>
+				<input id="business-name" value={businessName} onChange={handleBusiness} style={combinedSelectors} type="text" placeholder="Business name" name="displayName"/>
+				<input id="address1" value={address1} onChange={handleAddress1} style={styles.inputField} type="text" placeholder="Address line 1" name="address1"/>
+				<input id="address2" value={address2} onChange={handleAddress2} style={styles.inputField} type="text" placeholder="Address line 2" name="address2"/>
+				<input id="city" value={city} onChange={handleCity} style={styles.inputField} type="text" placeholder="City" name="city"/>
+				<input id="postcode" value={postcode} onChange={handlePostcode} style={combinedSelectors} type="text" placeholder="Postcode" name="postcode"/>
+				<input id="phone" value={phone} onChange={handlePhone} style={styles.inputField} type="number" placeholder="Phone number" name="phoneNumber"/>
+				<input id="email" value={email} onChange={handleEmail} style={styles.inputField} type="text" placeholder="Email address" name="email"/>
+				<input id="password" value={password} onChange={handlePassword} style={styles.inputField} type="text" placeholder="Password" name="password"/>
+				<Button type="submit">Submit</Button>
 			</form>
 			{user && <Redirect to="loginsuccess" />}
 		</div>
 	);
 }
+
+const styles = {
+	form: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#3F4552",
+		border: "2px solid #3f524c",
+		borderRadius: "10px",
+		paddingBottom: "25px",
+		marginBottom: "30px",
+		fontSize: "20px",
+		color: "white"
+	},
+	header: {
+		fontSize: "15px",
+		padding: "8px"
+	},
+	inputField: {
+		margin: "3px",
+		padding: "3px",
+		width: "80%",
+		border: "0px",
+		borderRadius: "3px"
+	},
+	space: {
+		marginBottom: "10px"
+	}
+};
+
+const deepCopy = JSON.parse(JSON.stringify(styles.inputField));
+const combinedSelectors = Object.assign(deepCopy, styles.space);
+
+const Button = styled.button`
+	background-color: #3F524C;
+	box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+	border: 0px;
+	border-radius: 3px;
+	color: white;
+	font-size: 14px;
+	padding: 4px 13px 4px;
+	margin-top: 10px;
+	&:hover {
+		background-color: #4A615A;
+	  cursor: pointer;
+	}
+`;
+
 
 export default CreateBusiness;
