bfc343c85d21da9c0e33453ad7cf9b5823d96a5c
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-14 16:19:19 +0700

    CreateUser: Implement CSS as object
    Style the form in a similar way to the previous commit. Note that the CSS is all new as previously the CSS file associated with CreateBusiness also covered CreateUser.

--- src/components/CreateUser/CreateUser.jsx
+++ src/components/CreateUser/CreateUser.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
 import firebase from "firebase/app";
+import styled from "styled-components";
 import { postFormDataAsJson } from "./../../util/helpers";
 import { API_URL } from "./../../util/urls";
 
@@ -65,19 +66,62 @@ function CreateUser () {
 
 	return (
 		<div>
-			<form onSubmit={createUser} className="form">
-				<header className="header">Create Account</header>
-				<input id="first-name" value={firstName} onChange={handleFirstName} className="input-field" type="text" placeholder="First name" name="displayName"/>
-				<input id="last-name" value={lastName} onChange={handleLastName} className="input-field" type="text" placeholder="Last name" name="lastName"/>
-				<input id="phone" value={phone} onChange={handlePhone} className="input-field" type="number" placeholder="Phone number"name="phoneNumber"/>
-				<input id="email" value={email} onChange={handleEmail} className="input-field" type="text" placeholder="Email address" name="email"/>
-				<input id="password" value={password} onChange={handlePassword} className="input-field" type="text" placeholder="Password" name="password"/>
-				<input id="postcode" value={businessId} onChange={handleBusinessId} className="input-field" type="number" placeholder="Business ID" name="businessId"/>
-				<button type="submit" className="submit">Submit</button>
+			<form onSubmit={createUser} style={styles.form}>
+				<header style={styles.header}>Create Account</header>
+				<input id="first-name" value={firstName} onChange={handleFirstName} style={styles.inputField} type="text" placeholder="First name" name="displayName"/>
+				<input id="last-name" value={lastName} onChange={handleLastName} style={styles.inputField} type="text" placeholder="Last name" name="lastName"/>
+				<input id="phone" value={phone} onChange={handlePhone} style={styles.inputField} type="number" placeholder="Phone number"name="phoneNumber"/>
+				<input id="email" value={email} onChange={handleEmail} style={styles.inputField} type="text" placeholder="Email address" name="email"/>
+				<input id="password" value={password} onChange={handlePassword} style={styles.inputField} type="text" placeholder="Password" name="password"/>
+				<input id="postcode" value={businessId} onChange={handleBusinessId} style={styles.inputField} type="number" placeholder="Business ID" name="businessId"/>
+				<Button type="submit">Submit</Button>
 			</form>
 			{user && <Redirect to="loginsuccess" />}
 		</div>
 	);
 }
+
+const styles = {
+	form: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#3F4552",
+		border: "2px solid #3f524c",
+		borderRadius: "10px",
+		paddingBottom: "25px",
+		marginBottom: "30px",
+		fontSize: "20px",
+		color: "white"
+	},
+	header: {
+		fontSize: "15px",
+		padding: "8px"
+	},
+	inputField: {
+		margin: "3px",
+		padding: "3px",
+		width: "80%",
+		border: "0px",
+		borderRadius: "3px"
+	}
+};
+
+const Button = styled.button`
+	background-color: #3F524C;
+	box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+	border: 0px;
+	border-radius: 3px;
+	color: white;
+	font-size: 14px;
+	padding: 4px 13px 4px;
+	margin-top: 10px;
+	&:hover {
+		background-color: #4A615A;
+	  cursor: pointer;
+	}
+`;
+
 
 export default CreateUser;
