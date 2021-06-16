0563b394398292962bdfaea46a8dcef1f744c831
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-16 13:48:20 +0700

    Helpers: Add validateSharedSignup function
    Add a method that validates the data from the part of the form that businesses and individual users share. Namely - phone, email and password.
    Use validator package for phone and email. For password, just check length.
    Currently uses HTML DOM methods to select the appropriate element. However, this needs to be changed as it doesn't work with React.

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,6 +1,8 @@
 /*
  * Various helper functions used throughout the project.
  */
+import validator from "validator";
+
 
 // Used to POST account creation data without using form attributes.
 export async function postFormDataAsJson ({ url, formData }) {
@@ -22,4 +24,37 @@ export async function postFormDataAsJson ({ url, formData }) {
 	}
 
 	return response.json();
+}
+
+
+// Validate the input that is common to both User and Business account creation.
+export function validateSharedSignup (phone, email, password) {
+	// Check field for valid input.
+	if (!validator.isMobilePhone(`${phone}`)) {
+		document.getElementById("phone").style.outline = "medium solid red";
+		document.getElementById("phone").value = "";
+		document.getElementById("phone").placeholder = "Please enter a valid mobile phone number";
+	// Remove red box around fields where valid input has been entered on next submission.
+	} else {
+		document.getElementById("phone").style.outline = 0;
+	}
+
+	if (!validator.isEmail(email)) {
+		document.getElementById("email").style.outline = "medium solid red";
+		document.getElementById("email").value = "";
+		document.getElementById("email").placeholder = "Please enter a valid email";
+	} else {
+		document.getElementById("email").style.outline = 0;
+	}
+
+	if (password.length < 8) {
+		document.getElementById("password").style.outline = "medium solid red";
+		document.getElementById("password").value = "";
+		document.getElementById("password").placeholder = "Passwords should be at least 8 chars long";
+	} else {
+		document.getElementById("password").style.outline = 0;
+	}
+
+	// If all fields pass, then return true so message can be sent.
+	return (validator.isMobilePhone(phone) && validator.isEmail(email) && password.length > 7);
 }
