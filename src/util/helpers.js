ee1c1c5108edd63cb59afe5ddcea9f5a15404fbc
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-21 17:34:40 +0700

    helpers: Select appropriate elements in validateSharedSignup
    Now the the form DOM element has been selected, each child element within it can be selected and manipulated by it's position in the form array.

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -31,27 +31,27 @@ export async function postFormDataAsJson ({ url, formData }) {
 export function validateSharedSignup (phone, email, password, element) {
 	// Check field for valid input.
 	if (!validator.isMobilePhone(`${phone}`)) {
-		element.style.outline = "medium solid red";
-		element.value = "";
-		element.placeholder = "Enter a valid mobile number";
+		element[6].style.outline = "medium solid red";
+		element[6].value = "";
+		element[6].placeholder = "Enter a valid mobile number";
 	} else { // Remove red box around fields when valid input.
-		element.style.outline = 0;
+		element[6].style.outline = 0;
 	}
 
 	if (!validator.isEmail(email)) {
-		element.style.outline = "medium solid red";
-		element.value = "";
-		element.placeholder = "Please enter a valid email";
+		element[7].style.outline = "medium solid red";
+		element[7].value = "";
+		element[7].placeholder = "Please enter a valid email";
 	} else {
-		element.style.outline = 0;
+		element[7].style.outline = 0;
 	}
 
 	if (password.length < 8) {
-		element.style.outline = "medium solid red";
-		element.value = "";
-		element.placeholder = "Needs to be > 8 chars";
+		element[8].style.outline = "medium solid red";
+		element[8].value = "";
+		element[8].placeholder = "Needs to be > 8 chars";
 	} else {
-		element.style.outline = 0;
+		element[8].style.outline = 0;
 	}
 
 	// If all fields pass, then return true so message can be sent.
