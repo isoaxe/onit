0350cfdf39d030f2df804947b57f8beff2631541
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-24 16:43:03 +0700

    userRoute: Check that businessId exists
    Perform a check that the businessId which the user has submitted exists in the Firestore. If it does not, respond with an error.
    Currently does not operate correctly per the comment.

--- functions/src/user/userRoute.js
+++ functions/src/user/userRoute.js
@@ -10,6 +10,20 @@ router.post("/user", async (req, res) => {
 		const role = "user";
 		const { displayName, phoneNumber, email, password, lastName, businessId } = req.body;
 
+		// Check that businessId exists by querying the Firestore.
+		const ids = [];
+		const db = admin.firestore();
+		const users = db.collection("users");
+		const businessIds = await users.get();
+		businessIds.forEach(doc => {
+			const id = doc.id.split("businessId-")[1];
+			ids.push(id);
+		});
+		/* Currently always true as businessIds is empty array and does not fetch properly. */
+		if (!ids.includes(businessId)) {
+			return res.status(400).send({ error: "businessId not found" });
+		}
+
 		const { uid } = await admin.auth().createUser({
 			displayName,
 			phoneNumber,
@@ -20,7 +34,6 @@ router.post("/user", async (req, res) => {
 		admin.auth().setCustomUserClaims(uid, { role, businessId });
 
 		// Not all required user data can be stored by auth. Use Firestore instead.
-		const db = admin.firestore();
 		const user = db.collection("users").doc(`businessId-${businessId}`)
 			.collection("user").doc(`${displayName} ${lastName}`);
 		user.set({
@@ -33,6 +46,7 @@ router.post("/user", async (req, res) => {
 
 		res.status(200).send({ message: "User account created" });
 	} catch (err) {
+		console.log("we have an error");
 		handleError(res, err);
 	}
 });
