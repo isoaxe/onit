81e1a8b57636d69e6721443177e767f8ae2b23a5
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-06-28 11:45:54 +0700

    Types: Add SignUpButtonProps type
    Add the interface and specify types for the label and onClick props.

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,10 +1,16 @@
 /*
  *  Interfaces to define object shapes for TypeScript.
  */
-import React from "react";
+import React, { MouseEvent } from "react";
 
 
 // Declare CSS as object within a TS file.
 export interface StyleSheet {
   [key: string]: React.CSSProperties;
+}
+
+// Prop shape for the SignUpButton component.
+export interface SignUpButtonProps {
+  label: string;
+  onClick: ((event: MouseEvent) => void) | undefined;
 }
