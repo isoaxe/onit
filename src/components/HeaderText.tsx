07b4cde2976135de983ca060bac07bbf3f5885b0
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-13 17:43:30 +0700

    HeaderText: Get user role
    Make a GET request to the '/role' endpoint. Then store user role to state and display in header.

--- src/components/HeaderText.tsx
+++ src/components/HeaderText.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "./../util/useAuth";
 import { textMain } from "./../util/colours";
+import { API_URL } from "./../util/urls";
 
 
 function HeaderText (): JSX.Element {
+	const [role, setRole] = useState(null);
 	const auth = useAuth();
+	const user = auth.user;
+
+	user.getIdToken(true).then(function (token) {
+		const requestOptions = {
+			method: "GET",
+			headers: { authorization: `Bearer ${token}` }
+		};
+		fetch(`${API_URL}/role/${user.uid}`, requestOptions)
+			.then(res => res.json())
+			.then(userRole => setRole(userRole.role));
+	}).catch(function (error) {
+		console.log(`An error occured whilst fetching user role: ${error}`);
+	});
 
 	return (
 		<Text>
-			Welcome, {auth.user.displayName} | Access level: user | Business name here
+			Welcome, {user.displayName} | Access level: {role} | Business name here
 		</Text>
 	);
 }
