5dded0fa58bc8aa234ae0fa174c090a1887b64f3
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-13 17:39:24 +0700

    Index: Add role routing
    Implement the newly created roleRoute.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,7 @@ import { routesConfig } from "./users/routesConfig";
 import loginSuccessRoute from "./loginSuccess/loginSuccess";
 import businessRoute from "./business/businessRoute";
 import userRoute from "./user/userRoute";
+import roleRoute from "./role/roleRoute";
 
 
 // Initialise the firebase-admin SDK in order to access its services.
@@ -26,6 +27,8 @@ app.use(loginSuccessRoute);
 app.use(businessRoute);
 // Set handler for individual user accounts.
 app.use(userRoute);
+// Set handler for user role setting and fetching.
+app.use(roleRoute);
 
 // Set the handlers for each http verb.
 routesConfig(app);
