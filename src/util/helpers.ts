f6238d23a3ef00c4359b514b0874ffd24fcf3a18
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-14 09:21:11 +0700

    helpers: Add role fetching logic
    Add the role fetching logic here as a separate function. This allows it to be called as needed.

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,6 +1,7 @@
 /*
  * Various helper functions used throughout the project.
  */
+import { API_URL } from "./../util/urls";
 
 
 // Used to POST account creation data without using form attributes.
@@ -23,4 +24,20 @@ export async function postFormDataAsJson ({ url, formData }) {
 	}
 
 	return response.json();
+}
+
+
+// GET assigned role for user.
+export function getRole (user) {
+	user.getIdToken(true).then(function (token) {
+		const requestOptions = {
+			method: "GET",
+			headers: { authorization: `Bearer ${token}` }
+		};
+		fetch(`${API_URL}/role/${user.uid}`, requestOptions)
+			.then(res => res.json())
+			.then(userRole => { return userRole.role; });
+	}).catch(function (error) {
+		console.log(`An error occured whilst fetching user role: ${error}`);
+	});
 }
