aae14bdf6216610c48c2de4e55fbd5c0043120b8
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-26 17:52:53 +0700

    userRoute: Add route for 'all' function
    Add routing so that the 'all' function is run when a GET request to the '/user' endpoint is sent.

--- functions/src/user/userRoute.ts
+++ functions/src/user/userRoute.ts
@@ -1,10 +1,14 @@
 import { Application } from "express";
-import { create } from "./controller";
+import { create, all } from "./controller";
 
 
 export function userRoute (app: Application): void {
 	// Create a new business user.
 	app.post("/user",
 		create
+	);
+	// Fetch all users within the business.
+	app.get("/user",
+		all
 	);
 }
