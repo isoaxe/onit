c5bd34c20973ce7bcefd44785bbeaa68588c6f28
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-26 17:31:22 +0700

    People: Add getUsers function
    Send a GET request to the '/user' endpoint in order to fetch all users for the company.

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -1,8 +1,24 @@
+import firebase from "firebase/app";
+import { API_URL } from "./../util/urls";
 import { textMain } from "./../util/colours";
 import { StyleSheet } from "./../util/types";
 
 
 function People (): JSX.Element {
+
+	function getUsers () {
+		firebase.auth().currentUser.getIdToken(true).then(function (token) {
+			const requestOptions = {
+				method: "GET",
+				headers: { authorization: `Bearer ${token}` }
+			};
+			fetch(`${API_URL}/user`, requestOptions)
+				.then(res => res.json())
+				.then(data => console.log(data));
+		}).catch(function (error) {
+			console.log(`GET request to /user failed: ${error}`);
+		});
+	}
 
 	return (
 		<div style={styles.root}>
