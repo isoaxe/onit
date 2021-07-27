6aee44fdca364a968ebc419790ea3687b0820d68
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-27 14:25:53 +0700

    People: Wrap getUsers in try block
    Wrap the getUsers function in a try / catch block. This replaces the chained .catch method to the .then statements. It will allow the subsequent replacement of .then with async / await.

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -7,17 +7,19 @@ import { StyleSheet } from "./../util/types";
 function People (props): JSX.Element {
 
 	function getUsers () {
-		firebase.auth().currentUser.getIdToken(true).then(function (token) {
-			const requestOptions = {
-				method: "GET",
-				headers: { authorization: `Bearer ${token}` }
-			};
-			fetch(`${API_URL}/user/${props.businessId}`, requestOptions)
-				.then(res => res.json())
-				.then(data => console.log(data));
-		}).catch(function (error) {
+		try {
+			firebase.auth().currentUser.getIdToken(true).then(function (token) {
+				const requestOptions = {
+					method: "GET",
+					headers: { authorization: `Bearer ${token}` }
+				};
+				fetch(`${API_URL}/user/${props.businessId}`, requestOptions)
+					.then(res => res.json())
+					.then(data => console.log(data));
+			});
+		} catch (error) {
 			console.log(`GET request to /user failed: ${error}`);
-		});
+		}
 	}
 
 	getUsers();
