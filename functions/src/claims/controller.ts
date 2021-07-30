fd27d6e508f4dd2d4c3d4f133962df5d86a478b7
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-30 16:46:53 +0700

    controller: Wrap get function in try / catch block
    + Add return keyword to the response.
    + Wrap code in try block.
    + Add a standard error helper function.
    + Implement the helper in a catch block.

--- functions/src/claims/controller.ts
+++ functions/src/claims/controller.ts
@@ -4,7 +4,16 @@ import { Request, Response } from "express";
 
 // Find custom claims set for user.
 export async function get (req: Request, res: Response) {
-	const { id } = req.params;
-	const userRecord = await admin.auth().getUser(id);
-	res.status(200).send(userRecord.customClaims);
+	try {
+		const { id } = req.params;
+		const userRecord = await admin.auth().getUser(id);
+		return res.status(200).send(userRecord.customClaims);
+	} catch (err) {
+		return handleError(res, err);
+	}
+}
+
+// Standard error helper function.
+function handleError (res: Response, err: Error) {
+	return res.status(500).send({ error: `${err}` });
 }
