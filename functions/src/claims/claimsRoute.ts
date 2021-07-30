a16868339cfc113a9e3e0e4f86ee2c6dab5ad585
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-30 16:36:00 +0700

    claimsRoute: Convert to TS
    Convert the filetype to TypeScript by changing the extension.
    Add param and return types for the exported function.

--- functions/src/claims/claimsRoute.ts
+++ functions/src/claims/claimsRoute.ts
@@ -1,7 +1,8 @@
+import { Application } from "express";
 import { get } from "./controller";
 
 
-export function claimsRoute (app) {
+export function claimsRoute (app: Application): void {
 	// GET custom claims for the user.
 	app.get("/claims/:id",
 		get
