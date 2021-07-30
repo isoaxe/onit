ad2a0f4990d8f828e0bfe31da9b2b1c0b870db63
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-30 16:31:41 +0700

    claimsRoute: Remove HTTP functions
    Remove the logic for the HTTP functions as this is now contained in controller.
    Just leave HTTP routing here with the the functionality imported instead.

--- functions/src/claims/claimsRoute.js
+++ functions/src/claims/claimsRoute.js
@@ -1,16 +1,9 @@
-import * as admin from "firebase-admin";
-import { Router } from "express";
-
-const router = Router();
+import { get } from "./controller";
 
 
-// GET request to find custom claims set for user.
-router.get("/claims/:id", (req, res) => {
-	const { id } = req.params;
-	admin.auth().getUser(id)
-		.then((userRecord) => {
-			res.status(200).send(userRecord.customClaims);
-		});
-});
-
-export default router;
+export function claimsRoute (app) {
+	// GET custom claims for the user.
+	app.get("/claims/:id",
+		get
+	);
+}
