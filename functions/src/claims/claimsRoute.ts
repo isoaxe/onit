1fdba95e6ba73a0a690fc678b43ca4514e3f50ca
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-07-31 12:31:10 +0700

    claimsRoute: Add POST request handler
    Add a route that handles a POST request to the '/claims' endpoint given a user id. Runs the function defined in the previous commit when matched.

--- functions/src/claims/claimsRoute.ts
+++ functions/src/claims/claimsRoute.ts
@@ -1,10 +1,14 @@
 import { Application } from "express";
-import { get } from "./controller";
+import { get, change } from "./controller";
 
 
 export function claimsRoute (app: Application): void {
 	// GET custom claims for the user.
 	app.get("/claims/:id",
 		get
+	);
+	// POST request to change users role.
+	app.post("/claims/:uid",
+		change
 	);
 }
