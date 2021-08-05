bacaf9ce1fbac45a8ff1cd4141d7c7114c817a88
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-08-05 19:06:37 +0700

    Calendar: Expand event fields
    Added what should be the complete set of data required for a task in the app.
    Non-standard properties get put in the extendedProps attribute as recommended by the documentation.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -26,19 +26,30 @@ function Calendar () {
 					buttonText: "List Week",
 				},
 			}}
-			events={dummyEvents}
+			events={dummyTasks}
 			initialView="dayGridMonth"
 			height="88vh"
 		/>
 	);
 }
 
-const dummyEvents = [
+const dummyTasks = [
 	{
 		title: "R906 Plant watering",
+		allDay: true,
 		start: "2021-08-07",
-		end: "2021-08-08",
-	}
+		end: "2021-08-07",
+		extendedProps: {
+			message: "There are 8 plants in total. Use the full bucket of water.",
+			complete: false,
+			completionTime: "2021-08-07T13:00:00",
+			assignedTime: "2021-08-07T11:00:00",
+			assignee: "Dara Hensey",
+			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
+			assignor: "Aaron Quigley",
+			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
+		}
+	},
 ];
 
 
