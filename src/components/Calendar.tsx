ade5994dde0ad5637adf1cd7c71018eb0392fe3d
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-08-05 20:33:22 +0700

    Calendar: Import dummy tasks
    Remove the tasks array from the component. Import it from the tasks file instead. Better to separate concerns like this rather than litter the Calendar component.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,7 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
+import { dummyTasks } from "./../util/tasks";
 
 
 function Calendar () {
@@ -32,25 +33,6 @@ function Calendar () {
 		/>
 	);
 }
-
-const dummyTasks = [
-	{
-		title: "R906 Plant watering",
-		allDay: true,
-		start: "2021-08-07",
-		end: "2021-08-07",
-		extendedProps: {
-			message: "There are 8 plants in total. Use the full bucket of water.",
-			complete: false,
-			completionTime: "2021-08-07T13:00:00",
-			assignedTime: "2021-08-07T11:00:00",
-			assignee: "Dara Hensey",
-			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
-			assignor: "Aaron Quigley",
-			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
-		}
-	},
-];
 
 
 export default Calendar;
