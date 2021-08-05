23957c136ee2eb0b821fe4e3981aeb9975c24512
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-08-05 20:30:57 +0700

    Create tasks.ts
    Temporary file to hold the dummy task data used for testing events. Best to keep as a separate file as it will be greatly expanded.

---
+++ src/util/tasks.ts
@@ -0,0 +1,22 @@
+/*
+ * Dummy data to test out events in the calender.
+ */
+
+export const dummyTasks = [
+	{
+		title: "R906 Plant watering",
+		allDay: true,
+		start: "2021-08-07",
+		end: "2021-08-07",
+		extendedProps: {
+			message: "There are 8 plants in total. Use the full bucket of water.",
+			complete: false,
+			completionTime: "2021-08-07T13:00:00",
+			assignedTime: "2021-08-07T11:00:00",
+			assignee: "Dara Hensey",
+			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
+			assignor: "Aaron Quigley",
+			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
+		}
+	},
+];
