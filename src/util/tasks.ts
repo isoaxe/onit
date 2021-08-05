6930783a7b4cd7f42d0fc9b65e3996f06cd2fb15
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-08-05 21:14:27 +0700

    tasks: Add more data
    Add another 4 tasks to more fully test events. Correct an error in the original one.

--- src/util/tasks.ts
+++ src/util/tasks.ts
@@ -11,8 +11,72 @@ export const dummyTasks = [
 		extendedProps: {
 			message: "There are 8 plants in total. Use the full bucket of water.",
 			complete: false,
-			completionTime: "2021-08-07T13:00:00",
+			completionTime: null,
 			assignedTime: "2021-08-07T11:00:00",
+			assignee: "Dara Hensey",
+			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
+			assignor: "Aaron Quigley",
+			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
+		}
+	},
+	{
+		title: "Electrical repairs",
+		allDay: false,
+		start: "2021-08-07T09:00:00",
+		end: "2021-08-07T11:30:00",
+		extendedProps: {
+			message: "External electrical contractor to come and repair the generator.",
+			complete: true,
+			completionTime: "2021-08-07T12:15:00",
+			assignedTime: "2021-08-05T18:00:00",
+			assignee: "Dara Hensey",
+			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
+			assignor: "Aaron Quigley",
+			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
+		}
+	},
+	{
+		title: "R807 New tenant",
+		allDay: false,
+		start: "2021-08-08T14:00:00",
+		end: "2021-08-08T14:30:00",
+		extendedProps: {
+			message: "New tenant coming to view the property. Discuss leasing options.",
+			complete: false,
+			completionTime: null,
+			assignedTime: "2021-08-07T10:00:00",
+			assignee: "Dara Hensey",
+			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
+			assignor: "Aaron Quigley",
+			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
+		}
+	},
+	{
+		title: "Pest repellant in drain",
+		allDay: false,
+		start: "2021-08-09T08:00:00",
+		end: "2021-08-09T13:00:00",
+		extendedProps: {
+			message: "Spray pest repellant in apartment drains from ground to 8th floor inclusive.",
+			complete: false,
+			completionTime: null,
+			assignedTime: "2021-08-04T11:00:00",
+			assignee: "Dara Hensey",
+			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
+			assignor: "Aaron Quigley",
+			assignorUid: "79TP3KL4Gzd5X0OUfzKb4fkp6423",
+		}
+	},
+	{
+		title: "R503 Clean apartment",
+		allDay: true,
+		start: "2021-08-09",
+		end: "2021-08-09",
+		extendedProps: {
+			message: "Tenent moved out yesterday. Full clean of apartment required.",
+			complete: false,
+			completionTime: null,
+			assignedTime: "2021-08-05T12:15:00",
 			assignee: "Dara Hensey",
 			assigneeUid: "dtdVI82ktJdFH90B1vyH5mkfB6r2",
 			assignor: "Aaron Quigley",
