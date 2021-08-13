97b8e978fff330e246d827a373b92834f8f8f6e1
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-08-13 14:09:04 +0700

    Calendar: Add comments to eventClicked
    Clarify and expand the comments for this function.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -10,17 +10,19 @@ import "./css/Calendar.css";
 function Calendar () {
 	const [infoRowIndex, setInfoRowIndex] = useState(99);
 
+	// Event handling for when task is clicked in any view.
 	function eventClicked (info) {
 		if (info.view.type === "calendar") {
 			// Redirect to listview for day that event takes place.
 			info.view.calendar.changeView("dayList", info.event.start);
 		} else {
-			// Find listview row (event) that was clicked and insert row below.
+			// If in listview, display info below clicked task.
 			const table = document.getElementsByClassName("fc-list-table")[0] as HTMLTableElement;
 			// Remove previous infoRow if open.
 			if (infoRowIndex !== 99) {
 				table.deleteRow(infoRowIndex);
 			}
+			// Find row index of event that was clicked and create row for info below.
 			const rows = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
 			const rowArray = Array.from(rows);
 			const index = rowArray.findIndex((row) => row.textContent.includes(info.event.title));
