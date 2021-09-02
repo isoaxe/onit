9bf11a66ed7dd4439c63e8e90d7153fed8e83eef
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-09-02 12:09:47 +0700

    TaskModal: Add title and message
    Add fields for the task title and message within a form in the task modal. Restructure the JSX so it makes more sense. Style the fields and rename some selectors.

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -1,9 +1,20 @@
+import { useState, ChangeEvent } from "react";
 import Modal from "react-modal";
 import "./css/TaskModal.css";
 
 
 function TaskModal (props): JSX.Element {
+	const [title, setTitle] = useState("");
+	const [message, setMessage] = useState("");
 	Modal.setAppElement("#root");
+
+	function handleTitle (event: ChangeEvent<HTMLInputElement>): void {
+		setTitle(event.currentTarget.value);
+	}
+
+	function handleMessage (event: ChangeEvent<HTMLTextAreaElement>): void {
+		setMessage(event.currentTarget.value);
+	}
 
 	function close () {
 		props.setTaskModalVisible(false);
@@ -17,8 +28,12 @@ function TaskModal (props): JSX.Element {
 			className="content"
 			overlayClassName="overlay"
 		>
-			<div className="wrapper">
-				<div className="title">Add Task</div>
+			<div>
+				<header>Add Task</header>
+				<form className="form">
+					<input value={title} onChange={handleTitle} type="text" placeholder="Title" name="title" />
+					<textarea value={message} onChange={handleMessage} placeholder="Message" name="message" rows={4} />
+				</form>
 				<button onClick={close}>Close Modal</button>
 			</div>
 		</Modal>
