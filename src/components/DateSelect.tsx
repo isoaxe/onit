06557becc165f7cc20fcdade184638f2085eede1
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-09-08 19:33:39 +0700

    DateSelect: Add header text
    Add a header so it is clear what the user needs to do. Wrap all in <div> as required by JSX.

--- src/components/DateSelect.tsx
+++ src/components/DateSelect.tsx
@@ -6,7 +6,10 @@ import "./css/TaskModal.css";
 function DateSelect (props): JSX.Element {
 
 	return (
-		<DatePicker selected={props.startDate} onChange={props.handleStartDate} showTimeSelect dateFormat="dd/MM/yyyy - HH:mm" />
+		<div>
+			<header>Select Task Day and Time</header>
+			<DatePicker selected={props.startDate} onChange={props.handleStartDate} showTimeSelect dateFormat="dd/MM/yyyy - HH:mm" />
+		</div>
 	);
 }
 
