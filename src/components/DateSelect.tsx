74f1b49cb904dff1cbb103a71aaf38edd1135db4
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-09-08 19:49:14 +0700

    DateSelect: Render alternate data based on allDay
    The previously defined functionality gets rendered when allDay is false. If the event lasts all day, then an alternate case was added whereby time is not selectable.

--- src/components/DateSelect.tsx
+++ src/components/DateSelect.tsx
@@ -5,12 +5,21 @@ import "./css/DateSelect.css";
 
 function DateSelect (props): JSX.Element {
 
-	return (
-		<div>
-			<header>Select Task Day and Time</header>
-			<DatePicker selected={props.startDate} onChange={props.handleStartDate} showTimeSelect dateFormat="dd/MM/yyyy - HH:mm" />
-		</div>
-	);
+	if (props.allDay) {
+		return (
+			<div>
+				<header>Select Task Day</header>
+				<DatePicker selected={props.startDate} onChange={props.handleStartDate} dateFormat="dd/MM/yyyy" />
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<header>Select Task Day and Time</header>
+				<DatePicker selected={props.startDate} onChange={props.handleStartDate} showTimeSelect dateFormat="dd/MM/yyyy - HH:mm" />
+			</div>
+		);
+	}
 }
 
 
