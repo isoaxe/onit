882cd4f2587994ec949acb0c8511c43062df3e13
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-09-25 23:52:43 +0700

    DateSelect: Add duration field
    Add two fields to capture duration hours and minutes separately.
    Added to the DateSelect component instead of TaskModal as it is only rendered when allDay is true.

--- src/components/DateSelect.tsx
+++ src/components/DateSelect.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./css/DateSelect.css";
 
 
 function DateSelect (props): JSX.Element {
+	const [hours, setHours] = useState("");
+	const [minutes, setMinutes] = useState("");
+
+	function handleHours (event) {
+		setHours(event.currentTarget.value);
+	}
+
+	function handleMinutes (event) {
+		setMinutes(event.currentTarget.value);
+	}
 
 	if (props.allDay) {
 		return (
@@ -17,6 +28,11 @@ function DateSelect (props): JSX.Element {
 			<div>
 				<header>Select Task Day and Time</header>
 				<DatePicker selected={props.startDate} onChange={props.handleStartDate} showTimeSelect dateFormat="dd/MM/yyyy - HH:mm" />
+				<div className="duration">
+					<header>Duration:</header>
+					<input value={hours} onChange={handleHours} type="number" placeholder="Hours" name="hours" min="0" max="99" />
+					<input value={minutes} onChange={handleMinutes} type="number" placeholder="Minutes" name="minutes" min="0" max="59" />
+				</div>
 			</div>
 		);
 	}
