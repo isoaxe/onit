2fde9c66c02921a977c3484cf3dfdfb0c543b0de
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-11-12 20:49:28 +0700

    validation: Remove incorrectPassword
    This helper function is no longer required as the input element that it styled has been replaced my a MUI component.

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -171,9 +171,3 @@ export function idNotFound(form) {
   form[6].value = "";
   form[6].placeholder = "Id not found";
 }
-
-export function incorrectPassword(form) {
-  form[1].style.outline = inputError;
-  form[1].value = "";
-  form[1].placeholder = "Password not correct";
-}
