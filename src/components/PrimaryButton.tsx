1503241f1c563d81479edc82cd80a78049480928
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-11-22 23:18:52 +0700

    PrimaryButton: Add style object
    Add a style object and declare some margin since the buttons are right next to one another.

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -3,12 +3,15 @@ import Button from "@mui/material/Button";
 function PrimaryButton(props): JSX.Element {
   const { label, onClick, type, disabled } = props;
 
+  const style = { margin: "5px" };
+
   return (
     <Button
       variant="contained"
       onClick={onClick}
       type={type}
       disabled={disabled}
+      sx={style}
     >
       {label}
     </Button>
