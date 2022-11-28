565a2515253fd0b869dcb2fb67a51b98b27d9f4f
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-11-28 19:45:57 +0700

    PhoneNumber: Destructure props
    Destructure the props object so that values can be accessed directly.

--- src/components/PhoneNumber.tsx
+++ src/components/PhoneNumber.tsx
@@ -5,14 +5,16 @@ import "./css/PhoneNumber.css";
 const phoneInputStyles = { width: "79%", marginBottom: "15px" };
 
 function PhoneNumber(props: PhoneInputProps): JSX.Element {
+  const { name, value, onChange } = props;
+
   return (
     <PhoneInput
       placeholder="Phone number"
       international
       defaultCountry="TH"
-      name={props.name}
-      value={props.value}
-      onChange={props.onChange}
+      name={name}
+      value={value}
+      onChange={onChange}
       style={phoneInputStyles}
     />
   );
