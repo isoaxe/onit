527e835f65ae55cbd3113ee8d3dd5369948a956d
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-11-28 20:52:10 +0700

    PhoneNumber: Set border based on helper text
    Use helperText to check for error. If not present, no error and normal border. If present, an error has occurred and the border gets coloured red.

--- src/components/PhoneNumber.tsx
+++ src/components/PhoneNumber.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import PhoneInput, { PhoneInputProps } from "react-phone-number-input";
+import { inputError } from "../util/colours";
 import "react-phone-number-input/style.css";
 import "./css/PhoneNumber.css";
 
@@ -8,6 +10,18 @@ type PhoneNumberProps = PhoneInputProps & { helperText: string };
 
 function PhoneNumber(props: PhoneNumberProps): JSX.Element {
   const { name, value, onChange, helperText } = props;
+
+  useEffect(() => {
+    const input = document.getElementsByClassName(
+      "PhoneInputInput"
+    )[0] as HTMLElement;
+
+    if (helperText) {
+      input.style.outline = inputError;
+    } else {
+      input.style.outline = "1px solid #31353d";
+    }
+  });
 
   return (
     <PhoneInput
