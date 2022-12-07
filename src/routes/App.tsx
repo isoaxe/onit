a1f709009516bb5f9512b8e6403ca91e1f8e4199
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-07 11:48:44 +0700

    App: Create new theme for mui
    Make a theme that covers the entire project. Provides darkmode for now, styling MUI fields as desired.

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-
+import { createTheme, ThemeProvider } from "@mui/material";
 import { ProvideAuth } from "./../util/useAuth";
 import RestrictedRoute from "./../routes/RestrictedRoute";
 import Homepage from "./../pages/Homepage";
 import LoginSignup from "./../pages/LoginSignup";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App(): JSX.Element {
   useEffect(() => {
@@ -14,14 +20,16 @@ function App(): JSX.Element {
   return (
     <div>
       <ProvideAuth>
-        <Switch>
-          <RestrictedRoute path="/home">
-            <Homepage />
-          </RestrictedRoute>
-          <Route path="/">
-            <LoginSignup />
-          </Route>
-        </Switch>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <RestrictedRoute path="/home">
+              <Homepage />
+            </RestrictedRoute>
+            <Route path="/">
+              <LoginSignup />
+            </Route>
+          </Switch>
+        </ThemeProvider>
       </ProvideAuth>
     </div>
   );
